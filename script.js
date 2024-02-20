5804270e1f7d0f6939e38da679a767c266d548cd
brendan smith <smit70592@icloud.com>
2024-02-20 11:00:03 -0600

    added object flavors to be shown in a table in the console

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const userInputString = prompt(
   "Please enter some froyo flavors separated by commas.",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
+
+const flavors = {
+  vanilla: 3,
+  coffee: 2,
+  strawberry: 1,
+};
+
+console.table(flavors);
